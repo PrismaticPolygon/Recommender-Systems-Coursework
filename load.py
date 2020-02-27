f223c06c3b7a1b725a70e62f852fb670a08d9785
PrismaticPolygon <domwhite97@gmail.com>
2020-02-27 10:31:38 +0000

    Added automatic dataset download.

--- load.py
+++ load.py
@@ -1,4 +1,20 @@
 import pandas as pd
+import requests
+import os
+
+from io import BytesIO
+from zipfile import ZipFile
+
+if not os.path.exists("datasets"):
+
+    os.mkdir("datasets")
+
+    response = requests.get("http://www.cp.jku.at/people/schedl/data/MusicMicro/11.11-09.12/musicmicro.zip")
+    fileobj = BytesIO(response.content)
+
+    with ZipFile(fileobj) as zipfile:
+
+        zipfile.extractall("datasets")
 
 artists = pd.read_csv("datasets/artist_mapping.txt", delimiter="\t", index_col="artist-id")
 cities = pd.read_csv("datasets/city_mapping.txt", delimiter="\t", index_col="city-id")
@@ -7,6 +23,7 @@ tracks = pd.read_csv("datasets/track_mapping.txt", delimiter="\t", index_col="tr
 
 data = pd.read_csv("datasets/listening_data.txt", delimiter="\t", encoding="utf-8")
 
+# Strange issue with columns shifted left by 1 needs to be rectified.
 data = data.rename({
     "twitter-id": "user-id",
     "user-id": "month",
@@ -23,9 +40,9 @@ data = data.rename({
 data.index.name = "twitter-id"
 data = data.drop("drop", axis=1)
 
-data = data.join(artists, on="artist-id")
 data = data.join(cities, on="city-id")
 data = data.join(countries, on="country-id")
+data = data.join(artists, on="artist-id")
 data = data.join(tracks, on="track-id")
 
 data.to_csv("datasets/data.csv")

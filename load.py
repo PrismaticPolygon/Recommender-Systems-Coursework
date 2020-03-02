e4996daffc10fd6a74c77e03a24ff943fc7f847d
PrismaticPolygon <domwhite97@gmail.com>
2020-03-02 20:45:15 +0000

    Now optimising context weights

--- load.py
+++ load.py
@@ -16,33 +16,73 @@ if not os.path.exists("datasets"):
 
         zipfile.extractall("datasets")
 
-artists = pd.read_csv("datasets/artist_mapping.txt", delimiter="\t", index_col="artist-id")
+# I do want to reset the indexes for all
+
+def index(column):
+
+    indices = dict()
+
+    for value in column:
+
+        if value not in indices:
+
+            indices[value] = len(indices)
+
+    return indices
+
+
+# Re-index artists from 0 - n
+artists = pd.read_csv("datasets/artist_mapping.txt", delimiter="\t")
+artist_index_map = index(artists["artist-id"])
+
+artists["artist_id"] = artists["artist-id"].map(artist_index_map)
+artists = artists.drop("artist-id", axis=1)
+artists = artists.set_index("artist_id")
+
+# Cities and countries are already indexed 0 - n
 cities = pd.read_csv("datasets/city_mapping.txt", delimiter="\t", index_col="city-id")
 countries = pd.read_csv("datasets/country_mapping.txt", delimiter="\t", index_col="country-id")
-tracks = pd.read_csv("datasets/track_mapping.txt", delimiter="\t", index_col="track-id")
 
-data = pd.read_csv("datasets/listening_data.txt", delimiter="\t", encoding="utf-8")
+# Re-index tracks from 0 - n
+tracks = pd.read_csv("datasets/track_mapping.txt", delimiter="\t")
+track_index_map = index(tracks["track-id"])
 
-# Strange issue with columns shifted left by 1 needs to be rectified.
-data = data.rename({
-    "twitter-id": "user-id",
-    "user-id": "month",
-    "month": "weekday",
-    "weekday": "longitude",
-    "longitude": "latitude",
-    "latitude": "country-id",
-    "country-id": "city-id",
-    "city-id": "artist-id",
-    "track-id": "drop",
-    "artist-id": "track-id"
-}, axis=1)
+tracks["track_id"] = tracks["track-id"].map(track_index_map)
+tracks = tracks.drop("track-id", axis=1)
+tracks = tracks.set_index("track_id")
 
-data.index.name = "twitter-id"
-data = data.drop("drop", axis=1)
+# Use index_col to fix malformed file (delimiters at the end of each line)
+data = pd.read_table("datasets/listening_data.txt", index_col=False)
 
-data = data.join(cities, on="city-id")
-data = data.join(countries, on="country-id")
-data = data.join(artists, on="artist-id")
-data = data.join(tracks, on="track-id")
+# Map user_id from 0 to n
+user_index_map = index(data["user-id"])
+
+data["user_id"] = data["user-id"].map(user_index_map)
+data = data.drop("user-id", axis=1)
+
+# Replace artist-id and track-id with mappings
+data["artist_id"] = data["artist-id"].map(artist_index_map)
+data["track_id"] = data["track-id"].map(track_index_map)
+
+data = data.drop(["track-id", "artist-id", "twitter-id"], axis=1)
+
+# Drop NaN values. Some of the tracks and artists in listening_data.txt don't have equivalents in artist_mapping / track_mapping
+data = data.dropna()
+
+data = data.reset_index(drop=True)
+
+# I won't do any of the other shit.
+# There isn't much point, after all.
+
+print(data.columns)
+
+print(data)
+
+data = data.join(cities, on="city_id")
+data = data.join(countries, on="country_id")
+data = data.join(artists, on="artist_id")
+data = data.join(tracks, on="track_id")
+
+# Let's just do a bit.
 
 data.to_csv("datasets/raw.csv")
--- recommender.py
+++ recommender.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 from scipy.sparse.linalg import svds
+from scipy.optimize import minimize
 
 NUM_RATINGS = 10000     # len(df)
 NUM_CONTEXTUAL_FACTORS = 2
@@ -10,15 +11,13 @@ LAMBDA = 0.2
 
 # Much slower with dtype={"rating": bool}
 df = pd.read_csv("datasets/data.csv", dtype={"weekend": int})
-df = df.sample(NUM_RATINGS)
-df = df.reset_index(drop=True)
+df = df[:NUM_RATINGS]
 
 NUM_ITEMS = len(df["track_id"].unique())
 NUM_USERS = len(df["user_id"].unique())
 
 print("|D| = {}. The number of tracks.".format(NUM_ITEMS))
 print("|U| = {}. The number of users.".format(NUM_USERS))
-
 
 # Pivot so that we have one row per user and one column per track.
 R = df.pivot_table(index="user_id", columns="track_id", values='rating').fillna(0)
@@ -27,58 +26,18 @@ R = df.pivot_table(index="user_id", columns="track_id", values='rating').fillna(
 mean = np.array(R.mean(axis=1))
 demeaned = R.sub(mean, axis=0).fillna(0).values
 
-print(demeaned.shape)   # (7026, 6188). No. users x no. tracks. 
-
-# That can be our initial guess for k.
-# Then we keep going, round and round.
-# This is going to be pretty fuckin' expensive.
-# That's fine, of course.
-# This will take decades to train, mind you.
-# I don't have to unwrap V or Q, just recalculate k.
-
-# Perform singular value decomposition (SVD).
+# Perform SVD
 d = min(demeaned.shape[0] - 1, 25)
 V, SIGMA, Q = svds(demeaned, k=d)
 
-SIGMA = np.diag(SIGMA)
-
-print("\nV = |U| x d = {} x {}. Each row is the strength of association between a USER and features.".format(*V.shape))
-print("Q = |D| x d = {} x {}. Each row is the strength of association between an ITEM and features.".format(*Q.shape))
-
-# Calculate predictions and put into a dataframe
-predictions = np.dot(np.dot(V, SIGMA), Q) + mean.reshape(-1, 1)
-predictions = pd.DataFrame(predictions, columns=R.columns)
-predictions.index.names = ["user_id"]
-
-# Initialise B randomly
-B = np.random.rand(MAX_NUM_CONTEXTUAL_CONDITIONS, NUM_CONTEXTUAL_FACTORS)
-
-# This will assume a certain format. It might actually be nicer to have them in separate tables
-
 Q = Q.T
 
-# So we want to minimise this function. x must be a 1-D array, which we can then unravel.
-# Itself a complex operation, mind you!
+# Randomly initialise context weights
+B = np.random.rand(MAX_NUM_CONTEXTUAL_CONDITIONS, NUM_CONTEXTUAL_FACTORS)
 
-# We are going to need these shapes.
-# Hol' up. We're optimising V and Q also.
-# Which means we'll have to include it ALL.
-# Unfortunate.
-# So it's just B?
-# Or do I?
-# In theory, yes. So long as my function is fast... I think.
+def cost(B):
 
-# x = np.concatenate((V, Q, B), axis=None)  # Flattens matrices before concatenation
-#
-# def cost_II(x, R):
-#
-#     V = x[]
-
-
-
-# Is R fixed? Yes.
-
-def cost(R, V, Q, B):
+    B = B.reshape(MAX_NUM_CONTEXTUAL_CONDITIONS, NUM_CONTEXTUAL_FACTORS)
 
     cost_sum = 0
 
@@ -87,48 +46,28 @@ def cost(R, V, Q, B):
         index = row[0]
         u = row[1]  # user_id
         i = row[2]  # track_id
-        c = row[-NUM_CONTEXTUAL_FACTORS - 1:-1]  # context
+        c = [int(x) for x in row[-NUM_CONTEXTUAL_FACTORS - 1:-1]]  # context
         r = row[-1]  # rating
 
         v_u = V[index]
         q_i = Q[index]
 
-        term = (r - np.dot(v_u, q_i) - sum([B[c[k], k] for k in range(2)])) ** 2
+        context = sum([B[c[k], k] for k in range(2)])
+        term = (r - np.dot(v_u, q_i) - context) ** 2
 
         regularisation = LAMBDA * (np.dot(v_u, v_u) + np.dot(q_i, q_i) + np.sum(B ** 2))
 
         cost_sum += term + regularisation
 
+    print("{:.4f}".format(cost_sum))
+
     return cost_sum
 
 
-# SGD is sensitive to feature scaling
-# Converges after approx 10 ^ 6 training examples.
+result = minimize(cost, B, method="BFGS")
 
-print(V.shape)  # (7877, 25)
+B = result.x.reshape(MAX_NUM_CONTEXTUAL_CONDITIONS, NUM_CONTEXTUAL_FACTORS)
 
-q = V.shape
+# Need to extract result
 
-x = V.flatten() # (198700, )
-
-# We also want to optimise k, though. We do that separately I believe.
-
-print(x.shape)
-
-z = x.reshape(q)
-
-print(z.shape)
-
-# Easy as taking candy off a baby.
-# Okay.
-
-params = np.concatenate((V, Q, B), axis=None)  # Flattens matrices before concatenation
-
-# Excellent. 350,000 parameters. Very trainable.
-# Huh. I need my custom loss function.
-
-print(params.shape)
-
-# print(V.flatten().shape)
-
-# Parameters are in V, Q, b, and B.
+print(B)

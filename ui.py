6bf68febe1d3af59fa591f6decd2c485ae0e98f2
PrismaticPolygon <domwhite97@gmail.com>
2020-03-04 11:50:28 +0000

    Added IEEE report

---
+++ ui.py
@@ -0,0 +1,12 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("")
+
+# We'll want to be able to sign in as a user.
+# Didn't I say that I'd rip off web tech?
+# I think that it might be more effort than it is worth.
+# Yikes. I feel like a can't do anything! Thursday is three sleeps away.
+# I'm just tired. Tomorrow will be a good day
+# I could probably make my autoencoder diagram?

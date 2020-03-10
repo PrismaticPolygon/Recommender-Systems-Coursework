b5a251bdf9d303102dcd204c925cfa529ad2c6a5
PrismaticPolygon <domwhite97@gmail.com>
2020-03-10 21:35:51 +0000

    Removed moment

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
 from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
 bootstrap = Bootstrap()
-moment = Moment()
 
 
 def create_app(config_class=Config):
@@ -19,7 +17,6 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
     bootstrap.init_app(app)
-    moment.init_app(app)
 
     from app.main import bp as main_bp
 
